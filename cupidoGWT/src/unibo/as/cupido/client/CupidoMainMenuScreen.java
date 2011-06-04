059c5a49509d7ce9eaa7d7751f18304aed294bfb
Marco Poletti <poletti.marco@gmail.com>
2011-06-04 19:11:09 +0200

    CupidoMainMenuScreen: temporarily allow direct access to all the other screens.

--- cupidoGWT/src/unibo/as/cupido/client/CupidoMainMenuScreen.java
+++ cupidoGWT/src/unibo/as/cupido/client/CupidoMainMenuScreen.java
@@ -1,8 +1,11 @@
 package unibo.as.cupido.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PushButton;
 
 public class CupidoMainMenuScreen extends AbsolutePanel {
 	
@@ -15,15 +18,54 @@ public class CupidoMainMenuScreen extends AbsolutePanel {
 		this.username = username;
 		setHeight("700px");
 		setWidth("700px");
+		
+		// FIXME: Remove this. It was inserted for debugging purposes.
+		this.username = "pippo";
+
 		Label label = new HTML("<b>Main menu screen (TODO)</b>");
 		add(label, 300, 320);
+		
+		PushButton tableButton = new PushButton("Vai alla schermata Tavolo");
+		tableButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				screenSwitcher.displayTableScreen();
+			}
+		});		
+		add(tableButton, 300, 400);
+		
+		PushButton errorButton = new PushButton("Vai alla schermata Errore generico");
+		errorButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				screenSwitcher.displayGeneralErrorScreen(new IllegalStateException("n example error message"));
+			}
+		});		
+		add(errorButton, 300, 450);
+		
+		PushButton observedTableButton = new PushButton("Vai alla schermata Tavolo osservato");
+		observedTableButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				screenSwitcher.displayObservedTableScreen();
+			}
+		});		
+		add(observedTableButton, 300, 500);
+		
+		PushButton scoresButton = new PushButton("Vai alla schermata Punteggi");
+		scoresButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				screenSwitcher.displayScoresScreen();
+			}
+		});		
+		add(scoresButton, 300, 550);
 	}
 
 	/**
 	 * @return the current username, or null if the user was logged out.
 	 */
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return username;
 	}
 }
