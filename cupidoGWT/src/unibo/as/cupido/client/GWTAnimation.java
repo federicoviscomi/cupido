882e822c86c9547e3e1a80d1b8d2e85eeea0d0d1
Marco Poletti <poletti.marco@gmail.com>
2011-06-10 10:26:59 +0200

    cupidoGWT: add the GWTAnimation interface and the SimpleAnimation class.

---
+++ cupidoGWT/src/unibo/as/cupido/client/GWTAnimation.java
@@ -0,0 +1,30 @@
+package unibo.as.cupido.client;
+
+public interface GWTAnimation {
+
+	/**
+	 * @return The duration of the animation, in milliseconds.
+	 */
+	public int duration();
+
+	/**
+	 * Start the animation.
+	 * When it finishes, the callback is invoked.
+	 */
+	public void run(AnimationCompletedListener listener);
+	
+	/**
+	 * This is called just before starting the animation.
+	 */
+	public void onStart();
+	
+	/**
+	 * This is called when the animation is finished, but before calling
+	 * the listener.
+	 */
+	public void onComplete();
+	
+	static public interface AnimationCompletedListener {
+		public void onComplete();
+	}
+}
