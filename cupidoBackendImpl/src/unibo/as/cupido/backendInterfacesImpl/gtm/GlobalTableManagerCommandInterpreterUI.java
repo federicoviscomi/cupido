a96936711354b1de71868a4f31e42f0102a777fa
Federico Viscomi <federico.viscomi@studio.unibo.it>
2011-06-12 19:26:53 +0200

    made more robust LTM and GTM command interpreter

--- cupidoBackendImpl/src/unibo/as/cupido/backendInterfacesImpl/gtm/GlobalTableManagerCommandInterpreterUI.java
+++ cupidoBackendImpl/src/unibo/as/cupido/backendInterfacesImpl/gtm/GlobalTableManagerCommandInterpreterUI.java
@@ -46,6 +46,14 @@ import unibo.as.cupido.backendInterfacesImpl.table.LTMSwarm.Triple;
  */
 public class GlobalTableManagerCommandInterpreterUI {
 
+	private static final String FORMAT = "%-7.7s %-3.3s %-10.10s %-20.20s\n";
+	private static final String USAGE = String.format(FORMAT + FORMAT + FORMAT
+			+ FORMAT + FORMAT, "COMMAND", "OPT", "LONG_OPT", "DESCRIPTION",
+			"start", "", "", "start the GTM server", "exit", "", "",
+			"shutdown the GTM server", "list", "-l", "--localManagers",
+			"list all LTM managed by this GTM server", "list", "-t", "--table",
+			"list all table managed by this GTM server");
+
 	public static void main(String[] args) {
 		new GlobalTableManagerCommandInterpreterUI().execute();
 	}
@@ -71,37 +79,54 @@ public class GlobalTableManagerCommandInterpreterUI {
 						String[] command = parser.getRemainingArgs();
 						if (command.length == 1) {
 							if (command[0].equals("start")) {
-								globalTableManager = new GlobalTableManager();
-							} else if (command[0].equals("exit")) {
-								exit(0);
-							} else if (command[0].equals("list")) {
-								boolean listLTM = (parser
-										.getOptionValue(listLocalManagersOtion) == null ? false
-										: true);
-								boolean listTables = (parser
-										.getOptionValue(listTableOption) == null ? false
-										: true);
-								if (listLTM) {
-									Triple[] allLocalServer = globalTableManager
-											.getAllLTM();
-									System.out
-											.format("\n list af all local server follows:");
-									for (Triple localServer : allLocalServer) {
-										System.out.format("\n %25s",
-												localServer);
-									}
+								if (globalTableManager == null) {
+									globalTableManager = new GlobalTableManager();
+								} else {
+									System.out.println("GTM already started!");
 								}
-								if (listTables) {
-									Collection<Pair<Table, LocalTableManagerInterface>> tableList = globalTableManager
-											.getTableList();
-									System.out
-											.format("\n list af all tables follows:");
-									for (Pair<Table, LocalTableManagerInterface> table : tableList) {
-										System.out.format("\n %25s", table);
+							} else {
+								if (globalTableManager == null) {
+									System.out.println("start GTM first!");
+									System.out.println(USAGE);
+								} else {
+									if (command[0].equals("exit")) {
+										exit(0);
+									} else if (command[0].equals("list")) {
+										boolean listLTM = (parser
+												.getOptionValue(listLocalManagersOtion) == null ? false
+												: true);
+										boolean listTables = (parser
+												.getOptionValue(listTableOption) == null ? false
+												: true);
+										if (listLTM) {
+											Triple[] allLocalServer = globalTableManager
+													.getAllLTM();
+											System.out
+													.format("\n list af all local server follows:");
+											for (Triple localServer : allLocalServer) {
+												System.out.format("\n %25s",
+														localServer);
+											}
+										}
+										if (listTables) {
+											Collection<Pair<Table, LocalTableManagerInterface>> tableList = globalTableManager
+													.getTableList();
+											System.out
+													.format("\n list af all tables follows:");
+											for (Pair<Table, LocalTableManagerInterface> table : tableList) {
+												System.out.format("\n %25s",
+														table);
+											}
+										}
+										if (!listLTM && !listTables) {
+											System.out
+													.println(" nothing to list?");
+										}
+									} else {
+										System.out.println(command[0]
+												+ ": command not found");
+										System.out.println(USAGE);
 									}
-								}
-								if (!listLTM && !listTables) {
-									System.out.println(" nothing to list?");
 								}
 							}
 						} else {
