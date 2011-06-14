5df0da327ed70d7b1d82fd577f20730f67c1a3e5
Federico Viscomi <federico.viscomi@studio.unibo.it>
2011-06-14 15:13:36 +0200

    modified some comments. modified some parameters name but not types

--- cupidoBackendInterfaces/src/unibo/as/cupido/backendInterfaces/exception/DuplicateUserNameException.java
+++ cupidoBackendInterfaces/src/unibo/as/cupido/backendInterfaces/exception/DuplicateUserNameException.java
@@ -2,4 +2,8 @@ package unibo.as.cupido.backendInterfaces.exception;
 
 public class DuplicateUserNameException extends Exception {
 
+	public DuplicateUserNameException(String userName) {
+		super(userName);
+	}
+
 }
