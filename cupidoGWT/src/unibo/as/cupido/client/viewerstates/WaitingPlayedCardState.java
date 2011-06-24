9de26686e0efd5cefc146058ca1ef5f292088377
Marco Poletti <poletti.marco@gmail.com>
2011-06-24 16:42:56 +0200

    cupidoGWT: remove the RandomCardGenerator class.

--- cupidoGWT/src/unibo/as/cupido/client/viewerstates/WaitingPlayedCardState.java
+++ cupidoGWT/src/unibo/as/cupido/client/viewerstates/WaitingPlayedCardState.java
@@ -19,7 +19,6 @@ package unibo.as.cupido.client.viewerstates;
 
 import unibo.as.cupido.client.CardsGameWidget;
 import unibo.as.cupido.client.GWTAnimation;
-import unibo.as.cupido.client.RandomCardGenerator;
 import unibo.as.cupido.common.structures.Card;
 
 import com.google.gwt.event.dom.client.ClickEvent;
